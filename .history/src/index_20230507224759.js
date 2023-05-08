b0b3bf4d11eb35bf20fa9d2279d78689c912bc7f
steven kenneth <stevmili123@gmail.com>
2023-05-08 19:24:17 +0700

    update

--- .history/src/index_20230507224759.js
+++ .history/src/index_20230507224759.js
@@ -1,21 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { PrismaClient } = require("@prisma/client");
 
+const prisma = new PrismaClient();
 const app = express();
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
-app.use(express.json());
-
 app.get("/api", (req, res) => {
   res.send("Selamat datang di API akuh");
 });
 
-const productController = require("./product/product.controller");
-
-app.use("/products", productController);
+app.get("products", (req, res) => {});
 
 app.listen(PORT, () => {
   console.log("Express API running in port: " + PORT);
--- .history/src/index_20230508110827.js
+++ .history/src/index_20230508110827.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { PrismaClient } = require("@prisma/client");
 
+const prisma = new PrismaClient();
 const app = express();
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
-app.use(express.json());
-
 app.get("/api", (req, res) => {
   res.send("Selamat datang di API akuh");
 });
 
-const productController = require("./product/product.controller");
-
-app.use("/products", productController);
+app.get("/products", async (req, res) => {
+  const product = await prisma.product.findMany();
+  res.send(products);
+});
 
 app.listen(PORT, () => {
   console.log("Express API running in port: " + PORT);
